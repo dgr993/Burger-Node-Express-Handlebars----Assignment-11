c52204ad266caff0bf361c8d03a08f40b245c94a
Wuzup <dgr993@gmail.com>
2020-08-20 18:23:55 -0600

    basic structure for completing assignment as outlined on hw instructions complete. lines 1-197 of homework_instructions.md complete

---
+++ config/orm.js
@@ -0,0 +1,16 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+function selectAll() {
+
+};
+
+function insertOne() {
+
+};
+
+function updateOne() {
+
+};
+
+module.exports = orm;
