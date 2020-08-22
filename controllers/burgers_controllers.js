03c6bc84fe94c5b37a7c6c68c85f72bf0c8817b0
Wuzup <dgr993@gmail.com>
2020-08-22 14:33:20 -0600

    all works except devoured button and adding a burger

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,8 +1,8 @@
 var express = require("express");
 // Import the model (burger.js) to use its database functions.
-var burger = require("../models/burger.js");
 
 var router = express.Router();
+var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
